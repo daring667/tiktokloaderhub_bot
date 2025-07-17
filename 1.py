d380e0ed02beabe54f6850205eeaeb7526007f95
darling <darling@darling.darling>
2025-07-17 12:17:40 +0500

    Изменение структуры и добавление поддоержки yt_dlr4

---
+++ 1.py
@@ -0,0 +1,77 @@
+import os
+import time
+import re
+import requests
+from dotenv import load_dotenv
+from pyrogram import Client, filters
+from pyrogram.enums import ParseMode
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
+load_dotenv()
+
+API_ID = int(os.getenv("API_KEY"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_URL = os.getenv("BOT_URL", "tiktokbot")
+CHANNEL_URL = os.getenv("CHANNEL_URL", "")
+downloading_users = set()
+app = Client("tiktok_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+
+@app.on_message(filters.regex(r'https?://.*tiktok\.com/') & (filters.group | filters.private))
+async def download_tiktok(client, message):
+    user_id = message.from_user.id
+    if user_id in downloading_users:
+        await message.reply("⏳ Подожди, пока закончится предыдущая загрузка.")
+        return
+
+    downloading_users.add(user_id)  # 🛑 Блокируем повторный запрос
+
+    msg = await message.reply("⏳")
+    filename = None
+
+    try:
+        match = re.search(r'https?://\S+', message.text or "")
+        if not match:
+            await msg.edit("❌ Не могу найти ссылку.")
+            return
+
+        url = match.group(0)
+        api = "https://tikwm.com/api/"
+        await asyncio.sleep(1.1)  # ⏱ анти-лимит
+        res = requests.get(api, params={"url": url}, timeout=10)
+
+        try:
+            data = res.json()
+        except Exception:
+            await msg.edit("❌ TikWM API вернул неверный ответ.")
+            return
+
+        video_url = data.get("data", {}).get("play")
+        if not video_url:
+            error_message = data.get("msg", "Видео недоступно.")
+            await msg.edit(f"❌ Ошибка: {error_message}")
+            return
+
+        filename = f"{int(time.time())}.mp4"
+        with requests.get(video_url, stream=True) as r:
+            with open(filename, "wb") as f:
+                for chunk in r.iter_content(chunk_size=1024 * 1024):
+                    if chunk:
+                        f.write(chunk)
+
+        await message.delete()
+        await client.send_video(message.chat.id, video=filename)
+        await msg.delete()
+
+    except Exception as e:
+        await msg.edit(f"❌ Не получилось: {e}")
+
+    finally:
+        downloading_users.discard(user_id)  # ✅ Разблокировка
+        if filename and os.path.exists(filename):
+            os.remove(filename)
+
+
+if __name__ == "__main__":
+    app.run()
